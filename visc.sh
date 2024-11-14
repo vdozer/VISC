daac6e128a39636651e0627c7957b6433afb8f17
vozer <vozer@noreply.codeberg.org>
2024-11-14 18:18:42 +0000

    Update visc.sh

--- visc.sh
+++ visc.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
-
 clear
 
+# ASCII art with color
 echo -e "\e[34m===.     :==--==.  :-====-:    .:-====-.  "
 echo -e "\e[34m.+==    .+== ==+. ===-::-===  -===-::===- "
 echo -e "\e[34m :==-   ==+. ==+. +==-:. --- -===     --- "
@@ -17,23 +17,23 @@ echo -e "\e[1;37mValentin's I3 Shell Script\e[0m"
 if command -v pacman &>/dev/null; then
     echo -e "\e[1;36mDetected Arch Linux.\e[0m"
     sudo pacman -Syu --noconfirm
-    sudo pacman -S --noconfirm i3 alacritty feh vim git
+    sudo pacman -S --noconfirm i3 i3status i3lock dmenu xorg-server xorg-xinit alacritty feh vim git
 elif command -v apt &>/dev/null; then
     echo -e "\e[1;31mDetected Debian/Debian-based.\e[0m"
     sudo apt update && sudo apt upgrade -y
-    sudo apt install -y i3 alacritty feh vim git
+    sudo apt install -y i3 i3status i3lock dmenu xorg alacritty feh vim git
 elif command -v dnf &>/dev/null; then
     echo -e "\e[1;36mDetected Fedora.\e[0m"
     sudo dnf upgrade -y
-    sudo dnf install -y i3 alacritty feh vim git
+    sudo dnf install -y i3 i3status i3lock dmenu xorg-x11-server-Xorg xorg-x11-xinit alacritty feh vim git
 elif command -v zypper &>/dev/null; then
     echo -e "\e[1;32mDetected OpenSUSE.\e[0m"
     sudo zypper refresh
-    sudo zypper install -y i3 alacritty feh vim git
+    sudo zypper install -y i3 i3status i3lock dmenu xorg-x11-server xinit alacritty feh vim git
 elif command -v emerge &>/dev/null; then
     echo -e "\e[1;35mDetected Gentoo.\e[0m"
     sudo emerge --sync
-    sudo emerge -v i3 alacritty feh vim git
+    sudo emerge -v i3 i3status i3lock dmenu xorg-server xinit alacritty feh vim git
 else
     echo "Unsupported distribution. Please install packages manually."
     exit 1
@@ -51,7 +51,7 @@ cp -r ~/visc-temp/picom ~/.config/
 # Copy .vimrc and .bashrc to home directory
 cp ~/visc-temp/.vimrc ~/
 cp ~/visc-temp/.bashrc ~/
-
+cp ~/visc-temp/.xinitrc ~/
 # Clean up the temporary dotfiles folder
 rm -rf ~/visc-temp
 
@@ -73,4 +73,3 @@ if [ "$choice" = "Y" ] || [ "$choice" = "y" ] || [ -z "$choice" ]; then
 fi
 
 echo -e "\e[1;32mSetup complete! Thank you for using VISC\e[0m"
-
