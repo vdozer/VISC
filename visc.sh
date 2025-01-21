5ae4aeb171833cedb0695d007e8b27f908771ff5
Valentin <valentin.ozer@aol.com>
2025-01-21 13:02:04 +0000

    Big updates
    Fixed Gentoo command, added all the other things that are needed including Thunar and create Pictures folder for Flameshot.

--- visc.sh
+++ visc.sh
@@ -17,23 +17,23 @@ echo -e "\e[1;37mValentin's I3 Shell Script\e[0m"
 if command -v pacman &>/dev/null; then
     echo -e "\e[1;36mDetected Arch Linux.\e[0m"
     sudo pacman -Syu --noconfirm
-    sudo pacman -S --noconfirm i3 i3status i3lock dmenu xorg-server xorg-xinit alacritty feh vim git picom dex
+    sudo pacman -S --noconfirm i3 i3status dex xss-lock i3lock networkmanager network-manager-applet pulseaudio alacritty dmenu flameshot thunar picom feh ttf-dejavu
 elif command -v apt &>/dev/null; then
     echo -e "\e[1;31mDetected Debian/Debian-based.\e[0m"
     sudo apt update && sudo apt upgrade -y
-    sudo apt install -y i3 i3status i3lock dmenu xorg alacritty feh vim git picom dex
+    sudo apt install -y i3 i3status dex xss-lock i3lock network-manager network-manager-gnome pulseaudio-utils alacritty dmenu flameshot thunar picom feh fonts-dejavu-core
 elif command -v dnf &>/dev/null; then
     echo -e "\e[1;36mDetected Fedora.\e[0m"
     sudo dnf upgrade -y
-    sudo dnf install -y i3 i3status i3lock dmenu xorg-x11-server-Xorg xorg-x11-xinit alacritty feh vim git picom dex
+    sudo dnf install -y i3 i3status dex xss-lock i3lock NetworkManager NetworkManager-plasma pulseaudio-utils alacritty dmenu flameshot thunar picom feh dejavu-sans-mono-fonts
 elif command -v zypper &>/dev/null; then
     echo -e "\e[1;32mDetected OpenSUSE.\e[0m"
     sudo zypper refresh
-    sudo zypper install -y i3 i3status i3lock dmenu xorg-x11-server xinit alacritty feh vim git picom dex
+    sudo zypper install -y i3 i3status dex xss-lock i3lock NetworkManager NetworkManager-pantheon pulseaudio-utils alacritty dmenu flameshot thunar picom feh fonts-dejavu
 elif command -v emerge &>/dev/null; then
     echo -e "\e[1;35mDetected Gentoo.\e[0m"
     sudo emerge --sync
-    sudo emerge -v i3 i3status i3lock dmenu xorg-server xinit alacritty feh vim git picom dex
+    sudo emerge -v x11-wm/i3 x11-misc/i3status x11-misc/dex x11-misc/xss-lock x11-misc/i3lock net-misc/networkmanager media-sound/pulseaudio app-emulation/alacritty app-misc/dmenu media-gfx/flameshot xfce-extra/thunar x11-misc/picom media-gfx/feh media-fonts/dejavu
 else
     echo "Unsupported distribution. Please install packages manually."
     exit 1
@@ -55,6 +55,9 @@ cp ~/visc-temp/.xinitrc ~/
 
 # Clean up the temporary dotfiles folder
 rm -rf ~/visc-temp
+
+# Also make a picture folder for Flameshot. Screenshots will be stored here.
+mkdir Pictures
 
 # Prompt to install zsh and set as default shell
 read -p $'\e[1;33mInstall zsh and set as default shell? (Y/n) \e[0m' choice
